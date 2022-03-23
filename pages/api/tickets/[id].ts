a15f415620a65c515012b54a0a824d1e108451ae
Tiago Dionesto <tiagodws@users.noreply.github.com>
2022-03-23 18:20:58 +0100

    Update [id].ts

--- pages/api/tickets/[id].ts
+++ pages/api/tickets/[id].ts
@@ -6,10 +6,11 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     query: { id },
     method,
+    body,
   } = req;
 
   switch (method) {
-    case 'DELETE':
+    case 'DELETE': {
       const ticketId = Number(id);
       const found = await prisma.ticket.findUnique({
         where: {
@@ -21,20 +22,48 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(404).send({ error: { message: 'Not found' } });
       }
 
-      const deleteTicket = await prisma.ticket.delete({
+      await prisma.ticket.delete({
         where: {
           id: ticketId,
         },
       });
 
-      if (!deleteTicket) {
-        res.status(400).send({ error: { message: 'Bad request' } });
+      res.status(200).send({});
+      break;
+    }
+    case 'PATCH': {
+      const ticketId = Number(id);
+      const found = await prisma.ticket.findUnique({
+        where: {
+          id: ticketId,
+        },
+      });
+
+      if (!found) {
+        res.status(404).send({ error: { message: 'Not found' } });
       }
 
-      res.status(200).send({ data: deleteTicket });
+      const { status } = body;
+
+      if (!['OPEN', 'CLOSED'].includes(status)) {
+        res.status(400).send({ error: { message: 'Invalid status' } });
+      }
+
+      const updatedTicket = await prisma.ticket.update({
+        where: {
+          id: ticketId,
+        },
+        data: {
+          status,
+        },
+      });
+
+      res.status(200).send({ data: updatedTicket });
       break;
-    default:
-      res.setHeader('Allow', ['DELETE']);
+    }
+    default: {
+      res.setHeader('Allow', ['DELETE', 'PATCH']);
       res.status(405).end(`Method ${method} Not Allowed`);
+    }
   }
 };
